ef0257895b91df052d365e12e6b9a75873f8e5ec
wess ammar <wesswessest@gmail.com>
2023-10-23 21:08:30 +0100

    deploy the app to github

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
 });
 
 
-Route::get('/api/user/me', function () {
-    return request()->user();
-})->middleware('auth:api');
+// Route::get('/api/user/me', function () {
+//     return request()->user();
+// })->middleware('auth:api');
